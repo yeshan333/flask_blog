3f976e4d43508b092da19477fa3a133f3ac1b1e5
yeshan333 <1329441308@qq.com>
2019-04-02 22:30:32 +0800

    add database models

--- bluelog/models.py
+++ bluelog/models.py
@@ -1,1 +1,32 @@
 # -*- coding: utf-8 -*-
+
+from bluelog.extensions import db
+from datetime import datetime
+
+# 管理员模型
+class Admin(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+
+    blog_title = db.Column(db.String(60))
+    blog_sub_title = db.Column(db.String(100))
+    name = db.Column(db.String(30))
+    about = db.Column(db.Text)
+
+# 分类
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+    posts = db.relationship('Post', back_populates='category')
+
+# 文章
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship('Category', back_populates='posts')
+
+   
