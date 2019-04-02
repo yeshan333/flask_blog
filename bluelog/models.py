8c0bc9b1e035c58ccacff2989314aafa24995356
yeshan333 <1329441308@qq.com>
2019-04-03 00:09:11 +0800

    database models mod

--- bluelog/models.py
+++ bluelog/models.py
@@ -18,6 +18,7 @@ class Admin(db.Model):
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=True)
+    
     posts = db.relationship('Post', back_populates='category')
 
 # 文章
@@ -26,7 +27,27 @@ class Post(db.Model):
     title = db.Column(db.String(60))
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
     category = db.relationship('Category', back_populates='posts')
 
-   
+    comment = db.relationship('Comment', backref='post', cascade='all,delete-orphan')
+
+# 评论
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(30))
+    email = db.Column(db.String(254))
+    site = db.Column(db.String(255))
+    body = db.Column(db.Text)
+    from_admin = db.Column(db.Boolean, default=False)
+    reviewed = db.Column(db.Boolean, default=False) # 判断是否通过审核
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
+    
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    post = db.relationship('Post', back_populates='comments')
+    
+    # 回复
+    replied_id = db.Column(db.Integer, db.ForeignKey('comment.id'))
+    replied = db.relationship('Comment', back_populates='replies', remote_side=[id])
+    replies = db.relationship('Comment', back_populates='replied', cascade='all')   
