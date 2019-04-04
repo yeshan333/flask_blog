0a48278be2e945908697ea4049bd76e3f5d49341
yeshan333 <1329441308@qq.com>
2019-04-04 21:17:54 +0800

    Forms done !

--- bluelog/forms.py
+++ bluelog/forms.py
@@ -1,1 +1,51 @@
 # -*- coding: utf-8 -*-
+
+from flask_ckeditor import CKEditorField
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, ValidationError, TextAreaField, HiddenField 
+from wtforms.validators import DataRequired, Length, URL, Optional, Email
+
+from bluelog.models import Category
+
+# 登录表单
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(1,20)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(8,128)])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Log in')
+
+# 文章表单
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(1,60)])
+    category = SelectField('Category', coerce=int, default=1)
+    body = CKEditorField('Body', validators=[DataRequired()])
+    submit = SubmitField()
+
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        # 列表生成式，列表元素为元组，作为下拉列表选项
+        self.category.choices = [(category.id, category.name)
+                                   for category in Category.query.order_by(Category.name).all()]
+
+# 分类表单
+class CategoryForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(1,30)])
+    submit = SubmitField()
+
+    def validate_name(self, field):
+        if Category.query.filter_by(name=field.data).first():
+            raise ValidationError('Name already in use.')
+
+# 评论表单
+class CommentForm(FlaskForm):
+    author = StringField('Name', validators=[DataRequired(), Length(1, 30)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(1, 254)])
+    site = StringField('Site', validators=[Optional(), URL(), Length(0,255)])
+    body = TextAreaField('Comment', validators=[DataRequired()])
+    submit = SubmitField()
+
+# 管理员评论表单
+class AdminCommentForm(CommentForm):
+    author = HiddenField()
+    email = HiddenField
+    site = HiddenField()
