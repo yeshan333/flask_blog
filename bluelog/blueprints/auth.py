c718302fc5f4964f9462a45da4297730f9e0cc0e
yeshan333 <1329441308@qq.com>
2019-04-05 21:31:13 +0800

    Login templates, Login endpoint

--- bluelog/blueprints/auth.py
+++ bluelog/blueprints/auth.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, flash
+from flask_login import login_user, current_user, login_required, logout_user
+
+from bluelog.models import Admin
 from bluelog.forms import LoginForm
 from bluelog.utils import redirect_back
 
@@ -8,9 +11,28 @@ auth_bp = Blueprint('auth', __name__)
 
 @auth_bp.route('/login', methods=['GEt', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('blog.index'))
+    
     form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        remember = form.remember.data
+        admin = Admin.query.first()
+        if admin:
+            if username == admin.username and admin.validate_password(password):
+                login_user(admin, remember)
+                flash('Welcom back.', 'info')
+                return redirect_back()
+            flash('Invalid username or password.', 'warning')
+        else:
+            flash('No account.','warning')
     return render_template('auth/login.html', form=form)
 
 @auth_bp.route('/logout')
+@login_required
 def logout():
+    logout_user()
+    flash('Logout Sucess.','info')
     return redirect_back()
