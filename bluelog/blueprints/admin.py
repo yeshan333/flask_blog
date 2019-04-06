592a9b72dd5ba8e92268cdd7c4a9907c44258eeb
yeshan333 <1329441308@qq.com>
2019-04-06 14:28:44 +0800

    post manage: new post, edit post, delete post, post templates done !

--- bluelog/blueprints/admin.py
+++ bluelog/blueprints/admin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint, render_template, url_for, redirect
+from flask import Blueprint, render_template, url_for, redirect, request, flash
 from flask_login import login_required, current_user
 
 from bluelog.extensions import db
@@ -30,29 +30,59 @@ def settings():
     form.about.data = current_user.about
     return render_template('admin/settings.html', form=form)
 
+# ----------------------------------------------------------------------------------
+
 @admin_bp.route('/post/manage')
 @login_required
 def manage_post():
-    return render_template('/admin/manage_post.html')
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(page, per_page=current_app.config['BLUELOG_MANAGE_POST_PER_PAGE'])
+    posts = pagination.items
+    return render_template('/admin/manage_post.html', pagination=pagination, posts=posts)
 
 @admin_bp.route('/post/new', methods=['GET', 'POST'])
 @login_required
 def new_post():
     form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        body = form.body.data
+        category = Category.query.get(form.category.data)
+        post = Post(title=title, body=body, category=category)
+        db.session.add(post)
+        db.session.commit()
+        flash('Post created !', 'sucess')
+        return redirect(url_for('blog.show_post', post_id=post.id))
     return render_template('/admin.new_post.html', foorm=form)
 
 @admin_bp.route('/post/<int:post_id>/edit', methods=['GET', 'POST'])
 @login_required
 def edit_post(post_id):
     form = PostForm()
+    # 获取对应文章id
+    post = Post.query.get_or_404(post_id)
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.body = form.body.data
+        post.category = Category.query.get(form.category.data)
+        db.session.commit()
+        flash('Post updated.', 'success')
+        return redirect(url_for('blog.show_post', post_id=post.id))
+    form.title.data = post.title
+    form.body.data = post.body
+    form.category.data = post.category_id
     return render_template('/admin/edit_post.html', form=form)
 
 @admin_bp.route('/post/<int:post_id>/delete', methods=['POST'])
 @login_required
 def delete_post(post_id):
     post = Post.query.get(post_id)
-    post.delete()
+    db.session.delete(post)
+    db.session.commit()
+    flash('Post delete.','sucess')
     return redirect_back()
+
+# ----------------------------------------------------------------------------------
 
 @admin_bp.route('/post/<int:post_id>/set-comment', methods=['POST'])
 @login_required
@@ -75,6 +105,7 @@ def approve_comment(comment_id):
 def delete_comment(comment_id):
     return redirect_back()
 
+# ----------------------------------------------------------------------------------
 
 @admin_bp.route('/category/manage')
 @login_required
@@ -101,6 +132,7 @@ def edit_category(category_id):
 def delete_category(category_id):
     return redirect(url_for('.manage_category'))
 
+# ----------------------------------------------------------------------------------
 
 @admin_bp.route('/link/manage')
 @login_required
